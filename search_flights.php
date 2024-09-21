8d20310438e1ddf6536537f41f17b8d19816e606
Magdalena Sánchez <magdalena.7.sanchez@gmail.com>
2024-09-20 22:32:58 -0600

    Espero, modificacion final 11

--- search_flights.php
+++ search_flights.php
@@ -37,7 +37,7 @@ $conn->close();
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-    <div class="container1">
+    <div class="container">
     <h1>Resultados de Vuelos</h1>
 
     <?php if (!empty($flights)): ?>
